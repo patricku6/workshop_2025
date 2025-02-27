da72d8b42186ee21bbaeed9dca888eb03c6d2b9b
Patrick Udo <mail@patrickudo.nl>
2025-02-27 14:16:37 +0100

    super gave geile slider

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,11 +1,18 @@
-import {Button} from "@mantine/core";
 import Template from "./Template.jsx";
+import { Carousel } from '@mantine/carousel';
+import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
 
 export default function Home() {
     return (
         <>
             <div>
                 <Template />
+                <Carousel withIndicators height={200}>
+                    <Carousel.Slide>1</Carousel.Slide>
+                    <Carousel.Slide>2</Carousel.Slide>
+                    <Carousel.Slide>3</Carousel.Slide>
+                </Carousel>
             </div>
         </>
     );
