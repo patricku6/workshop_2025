b2c2df6b8455ca1098a18f658960cc8d5720090f
Chris-26 <chris.halfman@student.graafschapcollege.nl>
2025-03-13 16:09:34 +0100

    Auto stash before merge of "main" and "origin/main"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\VerifyAdmin;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -17,6 +19,12 @@ Route::get('/register', function () {
     return Inertia::render('Register');
 })->name('register');
 
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index')->middleware('auth');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+Route::post('/profile-update', [ProfileController::class, 'update'])->name('profile.update');
+
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::post('/create', [AuthController::class, 'create'])->name('create');
@@ -24,6 +32,8 @@ Route::post('/create', [AuthController::class, 'create'])->name('create');
 Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('authenticate');
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/isLoggedIn', [AuthController::class, 'isLoggedIn'])->name('isLoggedIn');
 
 Route::get('/admin/dashboard', function () {
     return Inertia::render('AdminDashboard');
